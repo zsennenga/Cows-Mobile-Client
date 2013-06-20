9b7eb4c9cfa450c94277a1597aad26546dd1a1b5
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-06-20 11:38:42 -0700

    Merged CasAuth+CasAuthWebView Created Skeleton for CasAuth

--- Cows-Mobile-Client/gen/com/zennenga/cows_mobile_client/R.java
+++ Cows-Mobile-Client/gen/com/zennenga/cows_mobile_client/R.java
@@ -24,22 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080002;
-        public static final int action_settings=0x7f080007;
+        public static final int LinearLayout1=0x7f080003;
+        public static final int action_settings=0x7f080008;
         public static final int button1=0x7f080000;
-        public static final int button2=0x7f080001;
-        public static final int spinner1=0x7f080004;
-        public static final int spinner2=0x7f080006;
-        public static final int textView1=0x7f080003;
-        public static final int textView2=0x7f080005;
+        public static final int button2=0x7f080002;
+        public static final int spinner1=0x7f080005;
+        public static final int spinner2=0x7f080007;
+        public static final int textView1=0x7f080004;
+        public static final int textView2=0x7f080006;
+        public static final int webView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_cas_auth=0x7f030000;
-        public static final int activity_cas_auth_web_view=0x7f030001;
-        public static final int activity_event_creation=0x7f030002;
-        public static final int activity_main=0x7f030003;
-        public static final int activity_room_event_view=0x7f030004;
-        public static final int activity_room_select=0x7f030005;
+        public static final int activity_event_creation=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_room_event_view=0x7f030003;
+        public static final int activity_room_select=0x7f030004;
     }
     public static final class menu {
         public static final int cas_auth=0x7f070000;
