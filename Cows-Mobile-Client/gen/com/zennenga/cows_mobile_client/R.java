3b0b54f7c870b0c8f15d407b706198db8288d881
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-06-20 11:56:22 -0700

    Fixing Lint Errors/Warnings

--- Cows-Mobile-Client/gen/com/zennenga/cows_mobile_client/R.java
+++ Cows-Mobile-Client/gen/com/zennenga/cows_mobile_client/R.java
@@ -26,13 +26,13 @@ public final class R {
     public static final class id {
         public static final int LinearLayout1=0x7f080003;
         public static final int action_settings=0x7f080008;
-        public static final int button1=0x7f080000;
+        public static final int button1=0x7f080001;
         public static final int button2=0x7f080002;
         public static final int spinner1=0x7f080005;
         public static final int spinner2=0x7f080007;
         public static final int textView1=0x7f080004;
         public static final int textView2=0x7f080006;
-        public static final int webView1=0x7f080001;
+        public static final int webView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_cas_auth=0x7f030000;
@@ -43,23 +43,26 @@ public final class R {
     }
     public static final class menu {
         public static final int cas_auth=0x7f070000;
-        public static final int cas_auth_web_view=0x7f070001;
-        public static final int event_creation=0x7f070002;
-        public static final int main=0x7f070003;
-        public static final int room_event_view=0x7f070004;
-        public static final int room_select=0x7f070005;
+        public static final int event_creation=0x7f070001;
+        public static final int main=0x7f070002;
+        public static final int room_event_view=0x7f070003;
+        public static final int room_select=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int building_prompt=0x7f050008;
+        public static final int back=0x7f050009;
+        public static final int building_prompt=0x7f050007;
+        public static final int building_select=0x7f05000c;
+        public static final int create_event=0x7f05000b;
         public static final int hello_world=0x7f050002;
-        public static final int room_prompt=0x7f050009;
+        public static final int room_prompt=0x7f050008;
+        public static final int room_select=0x7f05000d;
         public static final int title_activity_cas_auth=0x7f050003;
-        public static final int title_activity_cas_auth_web_view=0x7f050007;
         public static final int title_activity_event_creation=0x7f050004;
         public static final int title_activity_room_event_view=0x7f050006;
         public static final int title_activity_room_select=0x7f050005;
+        public static final int view_events=0x7f05000a;
     }
     public static final class style {
         /** 
