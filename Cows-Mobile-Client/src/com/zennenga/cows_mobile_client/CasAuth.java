5fd84c100e61dae32fa73273ccb77c220a13377c
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-06-21 11:52:13 -0700

    -Completed CAS Auth

--- Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/CasAuth.java
+++ Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/CasAuth.java
@@ -1,9 +1,11 @@
 package com.zennenga.cows_mobile_client;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.webkit.CookieManager;
@@ -15,14 +17,24 @@ public class CasAuth extends Activity {
 	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//Allow network in main thread
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
+		
+		//Android Stuff
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cas_auth);
+		
+		//Setup Webview
 		WebView myWebView = (WebView) findViewById(R.id.webView);
-		myWebView.loadUrl("http://cas.ucdavis.edu/cas/login");
+		myWebView.loadUrl("https://cas.ucdavis.edu/cas/login");
 		myWebView.getSettings().setJavaScriptEnabled(true);
+		myWebView.getSettings().setSavePassword(false);
+		myWebView.getSettings().setSaveFormData(false);
 		myWebView.setWebViewClient(new WebViewClient() {
 			   public void onPageFinished(WebView view, String url) {
-			        checkCookie(view);
+				   //check if was 200 else reload or something
+			       checkCookie(view);
 			   }
 		});
 	}
@@ -35,13 +47,14 @@ public class CasAuth extends Activity {
 	}
 	public void checkCookie(View v)	{
 		CookieManager cookieManager = CookieManager.getInstance();
-		final String cookie = cookieManager.getCookie("cas.ucdavis.edu");
-		if (cookie.contains("CASTGC"))	{
-			String[] parts = cookie.split("&");
+		final String cookie = cookieManager.getCookie("https://cas.ucdavis.edu/cas");
+		if (cookie != null) Log.e("Cookie", cookie);
+		if (cookie != null && cookie.contains("CASTGC"))	{
+			String[] parts = cookie.split("; ");
 			for (String part : parts)	{
 				String[] pieces = part.split("=");
 				if (pieces[0].equals("CASTGC"))	{
-					//handle pieces[1]
+					Log.e("Cookie",pieces[1]);
 					Intent i = new Intent(v.getContext(), MainActivity.class);
 					startActivity(i);
 				}
--- Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/Utility.java
+++ Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/Utility.java
@@ -1,25 +1,40 @@
 package com.zennenga.cows_mobile_client;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import android.util.Log;
 import android.webkit.CookieManager;
 
 public class Utility {
-	public static boolean deauth(String ticket)	{
+	public static boolean deauth(CookieManager c, String ticket)	{
 		DefaultHttpClient httpclient = new DefaultHttpClient();
-		HttpGet request = new HttpGet("https://cas.ucdavis.edu/cas/logout");
-		CookieManager cookieManager = CookieManager.getInstance();
-		cookieManager.setCookie("cas.ucdavis.edu","CASTGC="+ticket);
+		URI url = null;
+		c.removeAllCookie();
+		c.removeSessionCookie();
+		try {
+			url = new URI("https://cas.ucdavis.edu/cas/logout");
+		} catch (URISyntaxException e1) {
+			Log.e("Deauth","URIExeception");
+			e1.printStackTrace();
+			return false;
+		}
+		HttpGet request = new HttpGet();
+		request.setURI(url);
+		c.setCookie("https://cas.ucdavis.edu/cas","CASTGC="+ticket);
 		try {
 			httpclient.execute(request);
 		} catch (ClientProtocolException e) {
+			Log.e("Deauth", "ClientProtocolException");
 			e.printStackTrace();
 			return false;
 		} catch (IOException e) {
+			Log.e("Deauth", "IOException");
 			e.printStackTrace();
 			return false;
 		}
