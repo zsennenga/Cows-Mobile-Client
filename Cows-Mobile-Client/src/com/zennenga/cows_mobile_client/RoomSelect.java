68830c7f94242fe107561171f01eab4756e58409
its-bhee <its-bhee@ITS-FD-5PWJZV1.ou.ad3.ucdavis.edu>
2013-06-21 12:45:16 -0700

    modified strings and spinners

--- Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/RoomSelect.java
+++ Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/RoomSelect.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 
@@ -23,14 +24,31 @@ public class RoomSelect extends Activity implements OnItemSelectedListener {
 	}
 	
 	public void addListenerOnBuildingSelectSpinner() {
-		  buildingSelectSpinner = (Spinner) findViewById(R.id.buildingSelectSpinner);
-		  buildingSelectSpinner.setOnItemSelectedListener(this);
+		buildingSelectSpinner = (Spinner) findViewById(R.id.buildingSelectSpinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		R.array.buildingOptions, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		buildingSelectSpinner.setAdapter(adapter);
+		buildingSelectSpinner.setOnItemSelectedListener(this);
     }
 	
 	public void addListenerOnRoomSelectSpinner() {
-		  roomSelectSpinner = (Spinner) findViewById(R.id.roomSelectSpinner);
-		  roomSelectSpinner.setOnItemSelectedListener(this);
-  }
+		roomSelectSpinner = (Spinner) findViewById(R.id.roomSelectSpinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		R.array.roomOptions, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		roomSelectSpinner.setAdapter(adapter);
+		roomSelectSpinner.setOnItemSelectedListener(this);
+    }
+	
+	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
