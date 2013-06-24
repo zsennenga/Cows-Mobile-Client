2d7826042b6c4d75868533d946617fb54f2807c8
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-06-24 14:33:36 -0700

    Fixing ActivityForResult in casAuth/EventCreation Setting up button handlers for Reccurrence

--- Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/Recurrence.java
+++ Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/Recurrence.java
@@ -2,14 +2,22 @@ package com.zennenga.cows_mobile_client;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class Recurrence extends Activity {
+	String recurrenceIn;
+	String recurrenceOut = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recurrence);
+		this.recurrenceIn = getIntent().getStringExtra("recurrenceIn");
+		if (!this.recurrenceIn.equals(""))	{
+			String[] pieces = this.recurrenceIn.split("&");
+		}
 	}
 
 	@Override
@@ -17,6 +25,17 @@ public class Recurrence extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.recurrence, menu);
 		return true;
+	}
+	
+	public void saveHandler(View v)	{
+		Intent i = new Intent();
+		i.putExtra("recurrenceOut", this.recurrenceOut);
+		this.setResult(RESULT_OK,i);
+		finish();
+	}
+	
+	public void backHandler(View v)	{
+		
 	}
 
 }
