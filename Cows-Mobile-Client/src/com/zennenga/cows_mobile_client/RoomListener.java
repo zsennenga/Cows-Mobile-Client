0ccca4f68b40f59ebee806f440a1edfff1c524d7
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-07-18 09:13:38 -0700

    Added Room Listener

---
+++ Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/RoomListener.java
@@ -0,0 +1,27 @@
+package com.zennenga.cows_mobile_client;
+
+import com.zennenga.utility.Validator;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class RoomListener implements OnItemSelectedListener {
+
+	Validator v;
+	public RoomListener(Validator v)	{
+		this.v = v;
+	}
+	
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View arg1, int arg2, long arg3) {
+		if (!parent.getSelectedItem().toString().contains("All "))
+			this.v.setField("BuildingAndRoom", ":" + parent.getSelectedItem().toString());
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		return;
+	}
+
+}
