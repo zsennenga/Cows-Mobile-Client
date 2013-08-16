cee68775d3b2f989fe437bb702e59d6f7c53cf14
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-08-16 10:50:17 -0700

    Removed TODOs

--- Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/Recurrence.java
+++ Cows-Mobile-Client/src/com/zennenga/cows_mobile_client/Recurrence.java
@@ -62,14 +62,14 @@ public class Recurrence extends Activity {
 			@Override
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
-				// TODO Auto-generated method stub
+				
 
 			}
 
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before,
 					int count) {
-				// TODO Auto-generated method stub
+				
 
 			}
 
